d974fad3ad3a0c5d5abb4d2829db584791dcffb2
Satavisha Roy <satavisha.roy@gmail.com>
2018-05-08 04:05:47 -0400

    050818-v1

--- src/fortuneTellingApp.java
+++ src/fortuneTellingApp.java
@@ -7,8 +7,16 @@ public class fortuneTellingApp {
 		QuestionAsker ask = new QuestionAsker(input);
 		System.out.println("Enter your first name");
 		String firstName = input.nextLine();
+		if (firstName.equalsIgnoreCase("quit")) {
+			System.out.println("Nobody likes a quitter...");
+			System.exit(0);
+		}
 		System.out.println("Enter your last name");
 		String lastName = input.nextLine();
+		if (lastName.equalsIgnoreCase("quit")) {
+			System.out.println("Nobody likes a quitter...");
+			System.exit(0);
+		}
 		System.out.println("Enter your age");
 		boolean hasAge = false;
 		int userAge = 0;
@@ -17,10 +25,17 @@ public class fortuneTellingApp {
 				userAge = input.nextInt();
 				hasAge = true;
 			}catch(Exception e) {
-				input.nextLine();
+				String strAge = input.nextLine();
+				//if (Integer.parseInt(strAge)) {
+					if (strAge.equalsIgnoreCase("quit")) {
+						System.out.println("Nobody likes a quitter...");
+						System.exit(0);
+					
+				}else {
 				System.out.println("Sorry, please enter a valid number.");
 			}
-		}System.out.println("Please enter your birth month");
+		}
+	}System.out.println("Please enter your birth month");
 		
 		boolean hasBirthMonth = false;
 		int userBirthMonth = 0;
@@ -32,26 +47,49 @@ public class fortuneTellingApp {
 //					//throw new RuntimeException("Birth month is out of range.");
 //				} else {
 				hasBirthMonth = true;
-				System.out.println("Thank you for providing your birth month");
+				break;
+				//System.out.println("Thank you for providing your birth month");
 //				} 
 			}catch (RuntimeException e1) {
-					input.nextLine();
-					System.out.println("Sorry, please enter a valid number.");
-			}
+					String strBirthMonth = input.nextLine();
+					if (strBirthMonth.equalsIgnoreCase("quit")) {
+						System.out.println("Nobody likes a quitter...");
+						System.exit(0);
+					} else {
+						System.out.println("Sorry, please enter a valid number.");
+						}
+					}
 		} while (hasBirthMonth == false);
 	input.nextLine();
 	System.out.println("Enter your favorite ROYGBIV color. If you do not know what ROYGBIV is, please enter \"Help\" to get a list of the ROYGBIV colors.");
+	boolean hasFavColor = false;
 	String favColor = "";
-		try {
+	while  (hasFavColor == false) {	
 			favColor = input.nextLine();
+//			if (favColor.toLowerCase().equals("help")) {
+//				System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
+//			}
+		try {	
+			int favcol = Integer.parseInt(favColor);
+			//hasFavColor = true;
+			System.out.println("This does not make sense. Please enter a ROYGBIV color or enter \"Help\".");
+		continue;
+		} catch (Exception e2) {		
+		//	hasFavColor = true;
 			if (favColor.toLowerCase().equals("help")) {
 				System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
-			}
-		}catch (Exception e2) {
-			input.nextInt();
-			System.out.println("Please provide a ROYGBIV color or enter \"Help\" to learn more.");
+				continue;
+			} 
+			else if (favColor.equalsIgnoreCase("quit")) {
+				System.out.println("Nobody likes a quitter...");
+				System.exit(0);
+			}hasFavColor = true;
 		}
-	
+		 // break;
+	 // input.nextLine();
+	 // System.out.println("Please provide a ROYGBIV color or enter \"Help\" to learn more.");
+	}
+	 
 	
 //  boolean hasFavColor = false;
 //	String userFavColor;
@@ -61,8 +99,32 @@ public class fortuneTellingApp {
 //		}
 //	}
 	System.out.println("How many siblings do you have?");
-	int numUserSiblings = input.nextInt();
-	
+	String strUserSiblings = input.next();
+	int numUserSiblings = 0;
+//	if (strUserSiblings.equalsIgnoreCase("quit")) {
+//		System.out.println("Nobody likes a quitter...");
+//		System.exit(0);
+//	}
+	boolean hasSiblings = false;
+	while (hasSiblings == false)
+	{
+		try {
+			
+			numUserSiblings = Integer.parseInt(strUserSiblings);
+			hasSiblings = true;
+		}
+	catch (NumberFormatException ex) {
+		if (strUserSiblings.equalsIgnoreCase("quit")) {
+			System.out.println("Nobody likes a quitter...");
+			System.exit(0);
+		}
+		else  {
+		System.out.println("Please check and type in again.");
+			break;
+			
+		}
+	}
+	}	
 	int retirementAge = 0;
 	if (userAge % 2 == 0) {
 		retirementAge = 68;
@@ -111,9 +173,12 @@ public class fortuneTellingApp {
 		userBalance = "$673,974.41";
 	}else {
 		userBalance = "$0.00";
-	}
 	
-  }
+	}
+	System.out.println(firstName.toUpperCase() + " " + lastName.toUpperCase() + " will retire in " + retirementAge + " years with " + userBalance + " in the bank, a vacation home in " + vacationHome + ", and travel by " + transportMode + ".");
+
+  
+	}
 }
  
 
